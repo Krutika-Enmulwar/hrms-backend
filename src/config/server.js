976152eede01ac473c71abadd07d74b233ea915e
KrutikaEnmulwar <krutikaenmulwar@gmail.com>
2025-12-17 09:26:42 +0530

    setup auth and employee basic routes

--- src/config/server.js
+++ src/config/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const authRoutes = require('../routes/auth');
+const employeeRoutes = require('../routes/employees');
 
 function createServer() {
   const app = express();
   app.use(express.json());
+  app.use('/api/auth', authRoutes);
+  app.use('/api/employees', employeeRoutes);
 
   app.get('/', (req, res) => {
     res.send('HRMS backend running');
---
+++ src/routes/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/login', (req, res) => {
+  res.send('login route');
+});
+
+router.post('/register', (req, res) => {
+  res.send('register route');
+});
+
+module.exports = router;
